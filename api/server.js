50f7263836d8b8aec22f45d958b0cb123ee0be24
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-12-30 14:01:21 -0500

    server up and running

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.send('SHE WORKS!');
+});
+
+module.exports = server;
